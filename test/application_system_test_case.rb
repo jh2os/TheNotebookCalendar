40e39d44bc70b79a710daaa3eedc7c0a0fac5f6a
jh2os <jh2os24@gmail.com>
2026-09-19 20:23:54 -0400

    Docker and domain settings

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,13 +3,7 @@ require "tmpdir"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1200 ] do |options|
-    chrome_binary = ENV["CHROME_BINARY"] || %w[
-      /usr/bin/google-chrome
-      /usr/bin/chromium
-      /usr/bin/chromium-browser
-      /snap/bin/chromium
-    ].find { |path| File.executable?(path) }
-    options.binary = chrome_binary if chrome_binary
+    options.binary = "/snap/bin/chromium"
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--remote-debugging-port=0")
